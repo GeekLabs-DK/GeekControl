c540422c47010a4d22d5c2f624bc45a0d4f7fba8
Mikkel Kirkgaard Nielsen <memb_github@mikini.dk>
2015-10-15 22:54:30 +0000

    Initial commit with toggle, plus, minus buttons and VFD.

---
+++ BBB_funcs.sh
@@ -0,0 +1,86 @@
+# BBB_funcs.sh: shell script providing BeagleBone Black cape control functionality.
+#               Meant to be sourced into a main script.
+
+if [ -z $CAPELOG ]; then
+	CAPELOG=/dev/null
+fi
+
+__BBB_GPIO_MAX=117    # Highest GPIO3_21 = 117(dec)
+
+# $1 is pin no (in dec. eg. 60) to export
+# $2 is pin direction to set (in or out)
+# Errors are echoed to stdout
+function export_pin
+{
+	if [ ! -z "$1" -a $1 -le $__BBB_GPIO_MAX ]; then
+		if [ $2 == "in" -o $2 == "out" ]; then
+			if [ ! -e /sys/class/gpio/gpio$1 ]; then
+	        	        echo $1 > /sys/class/gpio/export
+        	        	sleep 0.3
+			else
+	                        echo  `date +%FT%T` gpio $1 already exported. | tee --append $CAPELOG
+			fi
+			echo $2 > /sys/class/gpio/gpio$1/direction
+			sleep 0.3
+		else
+			echo  `date +%FT%T` direction \"$2\" in gpio $1 invalid. | tee --append $CAPELOG
+		fi
+	else
+		echo  `date +%FT%T` gpio \"$1\" invalid.  | tee --append $CAPELOG
+	fi;
+}
+
+# $1 is pin no (in dec. eg. 60) to set
+# $2 is desired pin state (0 or 1)
+# Errors are echoed to stdout
+function set_pin ()
+{
+    if [ ! -z "$1" -a $1 -le $__BBB_GPIO_MAX ]; then
+	if [ $2 -eq 0 -o $2 -eq 1 ]; then
+	    echo $2 > /sys/class/gpio/gpio$1/value
+	else
+	    echo  `date +%FT%T` gpio $1 value $2 invalid.  | tee --append $CAPELOG
+	fi
+    else
+	echo  `date +%FT%T` gpio $1 invalid.  | tee --append $CAPELOG
+    fi;
+}
+
+# $1 is pin no (in dec. eg. 60) for which state is wanted
+# Result is echoed to stdout, -1 returned for invalid pin
+function get_pin ()
+{
+	if [ $1 -le $__BBB_GPIO_MAX ]; then
+		cat /sys/class/gpio/gpio$1/value
+	else
+		echo -1
+	fi;
+}
+
+
+# $1 is identifier for cape to configure (eg. BB-BONE-GPS-GPRS)
+function config_cape ()
+{
+        for CAPEMGR in 9 8 7 6 5 4 3 2 1 0 -1; do 
+                if [ -d /sys/devices/bone_capemgr.$CAPEMGR ]; then
+                        break
+                fi
+        done
+        if [ ! $CAPEMGR -eq -1 ]; then
+                echo `date +%FT%T` found cape manager at /sys/devices/bone_capemgr.$CAPEMGR.   | tee --append $CAPELOG
+                # Allocate bone ressources
+		local CAPE_GREP=`grep $1 /sys/devices/bone_capemgr.$CAPEMGR/slots`
+		if [ x"$CAPE_GREP" == x ]; then
+                	echo $1 2>/dev/null 1>/sys/devices/bone_capemgr.$CAPEMGR/slots
+			if [ ! $? -eq 0 ]; then
+				echo `date +%FT%T` error activating cap \"$1\". Did you install the dtbo-file and disable any conflicting features?
+				exit 
+			fi
+		else
+	                echo `date +%FT%T` cape already configured: \"$1\" | tee --append $CAPELOG
+		fi
+        else
+                echo `date +%FT%T` no cape manager found. Aborting config_cape of $1.  | tee --append $CAPELOG
+		exit
+        fi;
+}
---
+++ geekcontrol.sh
@@ -0,0 +1,119 @@
+#! /bin/bash
+
+. ./BBB_funcs.sh
+. ./apikey
+
+BTN_TGL=60
+BTN_MINUS=50
+BTN_PLUS=51
+LED_STATE=30
+
+config_cape BB-UART1
+VFD_SERIAL=/dev/ttyO1
+
+export_pin $LED_STATE out
+export_pin $BTN_TGL   in
+export_pin $BTN_MINUS in
+export_pin $BTN_PLUS  in
+
+#http://spaceapi.geeklabs.dk/?control=sensors&command=set&key=&sensors={%22state%22:{%22open%22:true}}
+#http://spaceapi.geeklabs.dk/?format=json
+function set_state ()
+{
+#  echo setting state: $1, present: $2
+#  echo apikey: $apikey
+  wget -q -O- "http://spaceapi.geeklabs.dk/?control=sensors&command=set&key=${apikey}&sensors={%22state%22:{%22open%22:${1}},%22sensors%22:{%22people_now_present%22:[{%22value%22:${2}}]}}"
+}
+
+function get_state
+{
+  json=`wget -q -O- http://spaceapi.geeklabs.dk/?format=json`
+  state=`echo $json | js -e 'process.stdin.on("data", function (line) {console.log(JSON.parse(line).state.open)});'`
+  geeks=`echo $json | js -e 'process.stdin.on("data", function (line) {console.log(JSON.parse(line).sensors.people_now_present[0].value)});'`
+  echo got state: $state, present $geeks
+}
+
+function write_vfd()
+{
+  echo -ne "${1}" >${VFD_SERIAL}
+}
+
+function update_vfd()
+{
+  write_vfd "\\nGeekControl"
+  if $state; then
+    write_vfd "     open"
+  else
+    write_vfd "   closed"
+  fi
+  write_vfd "  Geeks present: ${geeks}"
+}
+
+
+last_plus=0
+last_minus=0
+last_tgl=0
+
+while true; do
+  get_state
+  if $state; then
+    set_pin $LED_STATE 1
+  else
+    set_pin $LED_STATE 0
+  fi
+  update_vfd
+
+  BUTCHECK=100
+  while [ $BUTCHECK -gt 0 ]; do
+  now_tgl=$(get_pin $BTN_TGL)
+  now_plus=$(get_pin $BTN_PLUS)
+  now_minus=$(get_pin $BTN_MINUS)
+  #echo $BUTCHECK:$now_tgl....
+
+  if [ $now_tgl -eq 1 ]; then
+    if [ $last_tgl -eq 0 ]; then
+      echo toggling, state: $state
+      if $state; then
+        state=false
+        geeks=0
+      else
+        state=true
+	geeks=1
+      fi
+      update_vfd
+      set_state $state $geeks
+      #BUTCHECK=0
+    fi
+  fi
+ 
+  if [ ! $BUTCHECK -eq 0 -a $now_plus -eq 1 ]; then
+    if [ $last_plus -eq 0 ]; then
+      echo plus, present: $geeks
+      geeks=$((${geeks}+1))
+      update_vfd
+      set_state true ${geeks}
+      #BUTCHECK=0
+    fi
+  fi
+
+  if [ ! $BUTCHECK -eq 0 -a $now_minus -eq 1 ]; then
+    if [ $last_minus -eq 0 ]; then
+      echo minus, present: $geeks
+      geeks=$((${geeks}-1))
+      if [ $geeks -le 0 ]; then
+        geeks=0
+        state=false
+      fi
+      update_vfd
+      set_state $state ${geeks}
+      #BUTCHECK=0
+    fi
+  fi
+
+  last_tgl=$now_tgl
+  last_plus=$now_plus
+  last_minus=$now_minus
+  sleep 0.1
+  BUTCHECK=$(($BUTCHECK-1))
+done
+done
